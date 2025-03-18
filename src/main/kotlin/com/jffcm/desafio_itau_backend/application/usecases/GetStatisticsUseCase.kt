3e84b4c354b0490b6ab11ba8219a808a5be7d8f3
João Francisco Franco Cançado Martins <joao.jffc@gmail.com>
2025-03-18 14:10:03 -0300

    refactor: rename parameter `seconds` to `intervalInSeconds` for clarity

--- src/main/kotlin/com/jffcm/desafio_itau_backend/application/usecases/GetStatisticsUseCase.kt
+++ src/main/kotlin/com/jffcm/desafio_itau_backend/application/usecases/GetStatisticsUseCase.kt
@@ -4,7 +4,7 @@ import com.jffcm.desafio_itau_backend.application.gateways.TransactionRepository
 import java.util.DoubleSummaryStatistics
 
 class GetStatisticsUseCase(private val transactionRepository: TransactionRepository) {
-    fun execute(seconds: Long): DoubleSummaryStatistics {
-        return transactionRepository.getStatistics(seconds)
+    fun execute(intervalInSeconds: Long): DoubleSummaryStatistics {
+        return transactionRepository.getStatistics(intervalInSeconds)
     }
 }
