5ae7142e5c700fb7dad24d8d87a4ea27dca89616
João Francisco Franco Cançado Martins <joao.jffc@gmail.com>
2025-03-18 13:42:30 -0300

    feat: add main application entry point

---
+++ src/main/kotlin/com/jffcm/desafio_itau_backend/DesafioItauBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.jffcm.desafio_itau_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesafioItauBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesafioItauBackendApplication>(*args)
+}
